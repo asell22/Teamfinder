e624309c91af11b98b8ceef4c8e22fd467abb0d0
asell22 <alexanderjsell@gmail.com>
2015-05-06 10:54:50 -0700

    add admin to users table, default set to false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331163243) do
+ActiveRecord::Schema.define(version: 20150506175257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20150331163243) do
   end
 
   create_table "users", force: true do |t|
-    t.string "email"
-    t.string "password_digest"
+    t.string  "email"
+    t.string  "password_digest"
+    t.boolean "admin",           default: false
   end
 
 end
